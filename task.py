b4803402be8ab0e0f02fba4628d96d3a5f53d2a3
HarveyT-LCWC <144792010+HarveyT-LCWC@users.noreply.github.com>
2024-12-03 14:31:26 +0000

    forloops

--- task.py
+++ task.py
@@ -5,14 +5,23 @@ def task_1(): # Lottery ticket generator
     ticket = []
 
     # Code here
-    
+    import random
+    for i in range (6):
+     number = randrange (1, 49)
+     ticket.append(number)
+     print("lottery numbers are:", ticket)
     return ticket
 
 def task_2(): # Countdown
 
     output = []
-
+   
     # Code here
+    
+    user = int(input("please enter a number from 1 to 100: "))
+        
+    for i in range(100, user - 1, -1):
+        output.append(i)
 
     return output
 
@@ -29,5 +38,12 @@ def task_3():
     car_make_lengths = ()
 
     # Code here
+    for car_make in people_cars.values:
+       car_make_lengths.add(len(car_make))
 
+    num_size = len(car_make_lengths)
+
+    print(f"There will be {num_size} different sizes of key rings.")
     return # Code the sentence here
+
+print(task_3())
